a667e495224a2e4504c106bf09bd5832e749b4bf
thayani-pereira <thayani_ti@hotmail.com>
2020-11-18 22:44:58 -0300

    Erro no cors

--- src/main/java/br/com/educanjos/infra/cors/CorsConfig.java
+++ src/main/java/br/com/educanjos/infra/cors/CorsConfig.java
@@ -8,9 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
+	@Override
     public void addCorsMappings( CorsRegistry registry ) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+        registry.addMapping("/**")        
+        .allowedMethods("GET","POST","PUT","PATCH","DELETE") 
+        .allowedOrigins("http://localhost:4200")
+        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                "Access-Control-Request-Headers");
+        
     }
 }

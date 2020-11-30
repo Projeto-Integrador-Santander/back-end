a33eb358c009c7759821ed496000a67bb28eb538
gisellykds <gisellykathellynsilva@gmail.com>
2020-11-29 23:49:22 -0300

    Atualizando credencial db2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into materia (descricao, materia, id) values
+("","Língua Portuguesa",1),
+("","Literatura",2),
+("","Filosofia",3),
+("","Sociologia",4),
+("","Química",5),
+("","Física",6),
+("","Biologia",7),
+("","Inglês",8);
